879e97da3900ffeec70d247a9657aefd846fab48
Serbay <Serbay@192.168.0.17>
2022-05-23 09:13:08 +0200

    renew

---
+++ SoSe2022_JavaGUI/src/defaultpackage/Main.java
@@ -0,0 +1,12 @@
+package defaultpackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * Hier einfach nochmal eure Aufgaben eintragen, die ihr euch zugefügt habt und der Meinung seid, dass ihr dies übernehmen möchtet
+		 */
+
+	}
+
+}
