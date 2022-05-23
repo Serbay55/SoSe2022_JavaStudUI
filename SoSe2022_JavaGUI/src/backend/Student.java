160488f1ca1ea0ef2c8730403ac4e13692e4d47b
Serbay <Serbay@192.168.0.17>
2022-05-23 12:29:04 +0200

    Stringbuilder in Name registering checks whether inputted string from scanner input line actually contains Strings and not numbers. If so it deletes the previous string and appends the cleand up one with the new string

--- SoSe2022_JavaGUI/src/backend/Student.java
+++ SoSe2022_JavaGUI/src/backend/Student.java
@@ -1,6 +1,8 @@
 package backend;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Student{
 	Scanner sc = new Scanner(System.in);
@@ -10,10 +12,53 @@ public class Student{
 	JavaSkill capability;
 	String hiredCompany;
 	
+	List studentenliste = new ArrayList();
+	
 	public void registerStudent(Student s) throws InputTypeError {
-		s.name = sc.nextLine();
+		s.setName(sc.nextLine());
 		
-		
+		/*s.setName(sc.nextLine());
+		s.setSurname(sc.nextLine());
+		s.setKurs(kurs);
+		s.setHiredCompany(sc.nextLine());
+		s.setCapability(capability);
+		studentenliste.add(s);*/
+	}
+	
+	public void secureStringsetter(Student s) {
+		char[] ch = s.name.toCharArray();
+		StringBuilder strbuild = new StringBuilder();
+		for(char c : ch) {
+			if(Character.isAlphabetic(c)) {
+				strbuild.append(c);
+			}
+		}
+		s.name = null;
+		s.name = strbuild.toString();
+	}
+
+	public Course getKurs() {
+		return kurs;
+	}
+
+	public void setKurs(Course kurs) {
+		this.kurs = kurs;
+	}
+
+	public JavaSkill getCapability() {
+		return capability;
+	}
+
+	public void setCapability(JavaSkill capability) {
+		this.capability = capability;
+	}
+
+	public String getHiredCompany() {
+		return hiredCompany;
+	}
+
+	public void setHiredCompany(String hiredCompany) {
+		this.hiredCompany = hiredCompany;
 	}
 
 	public String getName() {
