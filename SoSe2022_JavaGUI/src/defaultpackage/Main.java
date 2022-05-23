50ac306a6fb792c4cd751cf86d7903257097097c
Rafael-Simon <rafael.simon091998@gmail.com>
2022-05-23 09:35:39 +0200

    test

--- SoSe2022_JavaGUI/src/defaultpackage/Main.java
+++ SoSe2022_JavaGUI/src/defaultpackage/Main.java
@@ -5,7 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		/**
 		 * Hier einfach nochmal eure Aufgaben eintragen, die ihr euch zugefügt habt und der Meinung seid, dass ihr dies übernehmen möchtet
+		 *
+		 * Test
 		 */
+
 
 	}
 
